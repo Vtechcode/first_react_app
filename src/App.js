7bb272c9d6ce6dc14651b4f7a48323e232bee93e
Victor Waburi <victorwaburi@gmail.com>
2022-07-08 23:06:05 +0300

    Added index.css

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+//import Header from './components/Header'
+
+
+import Header from './components/Header'
+function App() {
+   const name = 'Vic';
+   const x = false;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+    
+  );
+}
+
+
+
+export default App;
